3c7ec9bf4b0ce3ead801cea474eff63fdda29692
Ilya <dert_94@mail.ru>
2020-10-09 08:31:24 +0300

    Add components

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import Main from "../main/main";
+import Login from "../login/login";
+import Offer from "../offer/offer";
+import Favorites from "../favorites/favorites";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 const App = (props) => {
 
   const {rentCount} = props;
 
   return (
-    <Main rentCount={rentCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main rentCount={rentCount} />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id" component={Offer} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
